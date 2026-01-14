801575c5aac9138f2252a107e1e3fcbe73d14497
Hungbocluaqua <chacchankophaihung123@gmail.com>
2026-01-14 16:43:03 +0700

    fix

--- TEC001/Assaignment 1/EX2.py
+++ TEC001/Assaignment 1/EX2.py
@@ -1,4 +1,4 @@
 import math
-#Tính chu vi hình tròn
+#Tinh chu vi hinh tron
 circumference = 2 * math.pi * float(input("Enter the radius of the circle: "))
 print("The circumference of the circle: ", f"{circumference: .2f} \n" )
