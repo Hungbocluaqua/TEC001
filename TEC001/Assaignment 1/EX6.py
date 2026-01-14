f32efc1bc61513bbb1d16708415d89f590008360
Hungbocluaqua <chacchankophaihung123@gmail.com>
2026-01-14 16:39:06 +0700

    Tách

--- TEC001/Assaignment 1/EX6.py
+++ TEC001/Assaignment 1/EX6.py
@@ -1,3 +1,4 @@
-﻿#Tạo dãy số cho ổ khóa 3 chữ số và 4 chữ số
+﻿import random
+#Tạo dãy số cho ổ khóa 3 chữ số và 4 chữ số
 print("\n3 digit lock combination: ", random.randint(0,9),random.randint(0,9),random.randint(0,9) )
 print("4 digit lock combination: ", random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9) )
