abea1a3ba358b6aef29b0373d957f585d2216ce9
Hungbocluaqua <chacchankophaihung123@gmail.com>
2026-02-26 08:58:41 +0700

    YEs

--- TEC001/Assignment 2/EX5.py
+++ TEC001/Assignment 2/EX5.py
@@ -1,9 +1,9 @@
 import math
 #Calculate the money value of two pizza
-dia1 = float(input("Enter the diameter of pizza 1: "))
-dia2 = float(input("Enter the diameter of pizza 2: "))
-prc1 = float(input("Enter the price of pizza 1: "))
-prc2 = float(input("Enter the price of pizza 2: "))
+dia1 = float(input("Enter the diameter of pizza 1 (m): "))
+dia2 = float(input("Enter the diameter of pizza 2 (m): "))
+prc1 = float(input("Enter the price of pizza 1 (USD): "))
+prc2 = float(input("Enter the price of pizza 2 (USD): "))
 piz1va = prc1/(math.pi*(dia1/2)**2)
 piz2va = prc2/(math.pi*(dia2/2)**2)
 if (piz1va) > (piz2va):
